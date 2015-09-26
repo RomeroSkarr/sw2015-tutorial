0bdb39960770b762af4ca8c4d26c36d328cdae4f
Gabriel Romero Ricardo <gabro.romero@gmail.com>
2015-09-26 23:57:24 +0200

    Done
    Single commit

--- src/tutorial.py
+++ src/tutorial.py
@@ -32,7 +32,7 @@ def sparql():
 
     endpoint = request.args.get('endpoint', None)
     query = request.args.get('query', None)
-
+    reasoning = request.args.get('reasoning','true')
     return_format = request.args.get('format','JSON')
 
 
@@ -47,7 +47,7 @@ def sparql():
             sparql.setReturnFormat(JSON)
             sparql.addParameter('Accept','application/sparql-results+json')
 
-        sparql.addParameter('reasoning','true')
+        sparql.addParameter('reasoning',reasoning)
 
         app.logger.debug('Query:\n{}'.format(query))
 
