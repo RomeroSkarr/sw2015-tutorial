ecc8ce373dc64e4bcb80e7dab98e0a79fa74c20b
Gabriel Romero Ricardo <gabro.romero@gmail.com>
2015-10-16 18:15:35 +0200

    Getting RDF and posting to stardog!

--- src/static/js/tutorial.js
+++ src/static/js/tutorial.js
@@ -1,90 +1,81 @@
 function initMap() 
 {
 	var map = new google.maps.Map(document.getElementById('map'), 
-		{
-			zoom: 2,
-			center: {lat: 0, lng: 0 }
-		}
-	);
+	{
+		zoom: 2,
+		center: {lat: 0, lng: 0 }
+	});
 	map.addListener('click', function(e) 
-		{
-			getPlacesWhereSocietalEventsHappenedFromCoordinates(e);
-   			map.panTo(e.latLng);
-		}
-	);
+	{
+			constructPlacesWhereSocietalEventsHappenedFromCoordinatesAndPost(e);
+			map.panTo(e.latLng);
+	});
 }
 
-function getPlacesWhereSocietalEventsHappenedFromCoordinates(e) 
+function constructPlacesWhereSocietalEventsHappenedFromCoordinatesAndPost(e) 
 {	
 	lat = e.latLng.lat();
 	lng = e.latLng.lng();
 	var query = 
-		"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> \n"+
-		"PREFIX dbo: <http://dbpedia.org/ontology/> \n"+
-		"SELECT ?place WHERE \n"+
-		"{\n"+
-		"    ?place a dbo:Place .\n"+
-		"    ?place geo:lat ?lat .\n"+
-		"    ?place geo:long ?long .\n"+
-		"    ?societalEvent a dbo:SocietalEvent . \n"+
-		"    ?societalEvent dbo:place ?place . \n"+
-		"    FILTER ( ?long > "+lng+" - 5 && ?long < "+lng+" + 5 && ?lat > "+lat+" - 5 && ?lat < "+lat+" + 5) \n"+
-		"} LIMIT 100";
-	//var endpoint = 'http://localhost:5820/tutorial/query';
+		"PREFIX dbr: <http://dbpedia.org/resource/> \n" + 
+		"PREFIX dbo: <http://dbpedia.org/ontology/> \n" + 
+		"PREFIX db: <http://dbpedia.org/> \n" + 
+		"CONSTRUCT \n" + 
+		"{ \n" + 
+		"	?place a dbo:Place . \n" + 
+		"} \n" + 
+		"WHERE \n" + 
+		"{ \n" + 
+		"	?societalEvent a dbo:SocietalEvent . \n" + 
+		"	?societalEvent dbo:place ?place . \n" + 
+		"	?place geo:lat ?lat . \n" + 
+		"	?place geo:long ?long . \n" + 
+		"	FILTER ( ?long > "+lng+" - 5 \n" + 
+		"		  && ?long < "+lng+" + 5  \n" + 
+		"		  && ?lat > "+lat+" - 5  \n" + 
+		"		  && ?lat < "+lat+" + 5) \n" + 
+		"}";
 	var endpoint = 'http://live.dbpedia.org/sparql';
-	var format = 'JSON';	
-	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format, 'reasoning': true}, function(json)
+	var format = 'RDF';	
+	$.get('/sparql',data={
+							'endpoint': endpoint, 
+						  	'query': query, 
+						  	'format': format, 
+						  }, function(data)
+	{
+		var pre = $('<pre></pre>');
+		pre.text(data);
+		$('#linktarget1').html(pre);
+		
+		// New, set an identifier on our <pre> tag		
+		pre.attr('id','linktarget1');
+				
+		var rdf_data = $('#linktarget1').text();				
+		$.post('/store',data={'data': rdf_data}, function(data)
 		{
-			console.log(json);	
-			try 
-			{
-				var vars = json.head.vars;	
-				console.log(vars);		
-				var ul = $('<ul></ul>');
-				ul.addClass('list-group');		
-				$.each(json.results.bindings, function(index,value)
-					{
-						var li = $('<li></li>');
-						li.addClass('list-group-item');			
-						$.each(vars, function(index, v)
-						{
-							var v_value = (value[v]['value']);
-							var name = v_value/*.substring(v_value.indexOf('/')+1)*/;
-							li.append("<table width=\"100%\">"+
-										"<tr>"+
-											"<td align=\":left\">"+
-												name.replace(/_/g," ")+
-											"</td>"+
-											"<td align=\"right\">"+
-												"<button onclick=\"getResultsOnMenuDiv()\">Teste</button>"+
-											"</td>"+
-										"</tr>"+
-									"</table>"+
-									"<br/>");	
-						});
-						ul.append(li);			
-					}
-				);			
-				$('#linktarget1').html(ul);
-			} 
-			catch(err) 
-			{
-				$('#linktarget1').html(err);
-			}		
-		}
-	);	
+			var pre = $('<pre></pre>');
+			pre.text(data);
+			$('#linktarget1').html(pre);
+		})		
+	});
 }
 
-function getResultsOnMenuDiv() 
+
+function getSpecificKindOfPlaceFromOurTripleStore(subclassOfPlaceWhereSocietalEventHappened) 
 {	
 	var query = "PREFIX map: <http://www.semanticweb.org/gabriel/ontologies/FinalProject#> \n"+
 				"SELECT ?x WHERE \n"+
 				"{ \n"+
-					"  ?x a map:PlaceWhereMilitaryConflictHappened . \n"+
+					"  ?x a map:"+subclassOfPlaceWhereSocietalEventHappened+" . \n"+
 				"}";
 	var endpoint = 'http://localhost:5820/tutorial/query';
 	var format = 'JSON';	
-	$.get('/sparql',data={'endpoint': endpoint, 'query': query, 'format': format, 'reasoning': true}, function(json)
+	$.get('/sparql',data={
+							'endpoint': endpoint, 
+							'query': query, 
+							'format': format, 
+							'reasoning': true
+						}, function(json)
 	{
 		console.log(json);		
 		try 
@@ -93,26 +84,24 @@ function getResultsOnMenuDiv()
 			var ul = $('<ul></ul>');
 			ul.addClass('list-group');		
 			$.each(json.results.bindings, function(index,value)
+			{
+				var li = $('<li></li>');
+				li.addClass('list-group-item');			
+				$.each(vars, function(index, v)
 				{
-					var li = $('<li></li>');
-					li.addClass('list-group-item');			
-					$.each(vars, function(index, v)
-						{
-							var v_value = (value[v]['value']);
-							var name = v_value.substring(v_value.indexOf('#')+1);
-							li.append("<table width=\"100%\">"+
-										"<tr>"+
-											"<td align=\":left\">"+
-												name.replace(/_/g," ")+
-											"</td>"+
-										"</tr>"+
-									"</table>"+
-									"<br/>");	
-						}
-					);
-					ul.append(li);			
-				}
-			);			
+					var v_value = (value[v]['value']);
+					var name = v_value.substring(v_value.indexOf('#')+1);
+					li.append("<table width=\"100%\">"+
+								"<tr>"+
+									"<td align=\":left\">"+
+										name.replace(/_/g," ")+
+									"</td>"+
+								"</tr>"+
+							"</table>"+
+							"<br/>");	
+				});
+				ul.append(li);			
+			});			
 			$('#linktarget2').html(ul);
 		} 
 		catch(err) 
